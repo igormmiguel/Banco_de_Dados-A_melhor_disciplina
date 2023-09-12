54e0be7198323e7963f9b9ce31fd5112bc591ece
Igor mm <igor.mmiguel@eaportal.org>
2023-09-12 11:04:12 -0300

    ex 4

--- lista.sql
+++ lista.sql
@@ -14,3 +14,10 @@ select  titulo, autor_id, nome
 from livros jkr 
 join autores a on a.id = jkr.autor_id
 where a.nome = 'J.K. Rowling';
+
+-- 4
+
+select aluno_id, nome, curso
+from alunos e
+left join matriculas m on m.id  = e.id
+where curso = "Engenharia de Software";
